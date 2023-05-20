328b69627e5dc4bc4373e05b762cc6e721e9aca8
Wailler-Prata <wailler.p.b@outlook.com>
2023-05-20 17:09:58 -0300

    teste de report
    X

---
+++ cypress/e2e/MySpecs/CyRequest.cy.js
@@ -0,0 +1,40 @@
+describe('Trabalhando com "Cy.Request"', () =>{
+    it('Requisitando um endereço por um CEP valido', ()=>{
+        cy.fixture('apiViaCep/enderecosValidos.json')
+        .then((data)=>{
+
+            const indice = Math.floor(Math.random() * ((data.length - 1) - 0 + 1) ) + 0 // Gera um numero de 0 a ultima posição do array de endereços
+            const raw = data[indice]
+
+            cy.request({
+                method: 'GET',
+                url: `ws/${raw.cep}/json`
+            })
+            .then((response)=>{
+                expect(response.status).to.eq(2010)
+                expect(raw).to.deep.equal(response.body)
+                //expect(response.body).to.have.keys(Object.keys(raw))
+            })            
+        })
+    })
+
+    it('Requisitando um endereço por um CEP inexistente', ()=>{
+        cy.request( 'GET',
+                    'ws/00000000/json',
+            ).then((response) =>{
+                expect(response.body.erro).to.eq(true)
+            })
+    })
+
+    it('Requisitando um endereço por um CEP inválido (Alfanumérico)', ()=>{
+        cy.request(
+            {
+                method: 'GET',
+                url: 'ws/95010A10/json',
+                failOnStatusCode: false
+            }
+        ).then((response) =>{
+            expect(response.status).to.eq(400)
+        })
+    })
+})
---
+++ cypress/e2e/MySpecs/teste.cy.js
@@ -0,0 +1,5 @@
+describe('teste', () =>{
+    it('visitando o google', ()=>{
+        cy.visit('https://www.google.com.br')
+    })
+})
